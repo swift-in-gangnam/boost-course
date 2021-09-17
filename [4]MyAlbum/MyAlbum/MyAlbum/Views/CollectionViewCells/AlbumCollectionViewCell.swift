cab9b0ecadfec7daa1eda35bbc543b5309a03c88
Jinyoung Kim <52317025+z3rosmith@users.noreply.github.com>
2021-09-17 20:24:00 +0900

    (add)MyAlbum

---
+++ [4]MyAlbum/MyAlbum/MyAlbum/Views/CollectionViewCells/AlbumCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  AlbumCollectionViewCell.swift
+//  MyAlbum
+//
+//  Created by Jinyoung Kim on 2021/09/17.
+//
+
+import UIKit
+
+class AlbumCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var countLabel: UILabel!
+}
